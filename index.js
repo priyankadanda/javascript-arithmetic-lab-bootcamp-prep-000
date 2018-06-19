a572f3f10bef83669635968d9668722181ee4d4c
Priyanka Danda <priyankadanda47@gmail.com>
2018-06-19 16:30:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,36 @@
+
+function add(a,b)
+{
+  return a+b
+}
+function subtract(a,b)
+{
+  return a-b
+}
+function multiply (a,b)
+{
+  return a*b
+}
+function divide()
+{
+ return a/b 
+}
+
+function inc (a)
+{
+  a= a++;
+  return a
+}
+function dec (a)
+{
+  a = a--;
+  return a
+}
+function makeInt(a)
+{
+  
+}
+function preserveDecimal(a)
+{
+  
+}
